fc59c26933e0bab75e026b1dbc090bbcc2114202
jpmaciel22 <131266912+jpmaciel22@users.noreply.github.com>
2025-02-10 16:10:52 -0300

    signals e outros foram adicionados mas ja tirados para trabalhar igual o angular antigo...

---
+++ 01-starting-project/src/app/user/user-antigo.component.ts
@@ -0,0 +1,26 @@
+import { Component, signal, computed } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+// const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  selectedUser = signal(DUMMY_USERS[0]) // isso é feito de forma análoga às locals do
+  // controller de JS, aq ele exporta as variaveis da userComponent como locals para serem usadas no seu
+  // proprio html, que depois sera rodado no app component e por ai vai..
+
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
+  // get imagePath() {
+  //   return 'assets/users/' + this.selectedUser.avatar
+  // }
+
+  onSelectUser(){
+    console.log('.')
+  }
+}
--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+// const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 
 @Component({
   selector: 'app-user',
@@ -7,6 +10,8 @@ import { Component } from '@angular/core';
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent {
+export class UserComponent {// isso é feito de forma análoga às locals do
+  onSelectUser(){
 
+  }
 }
