c9f2b2d07bbcdd6f142e725b12b4237f79bb5f62
jpmaciel22 <131266912+jpmaciel22@users.noreply.github.com>
2025-02-12 14:57:38 -0300

    Adicionando tasks de task

--- 01-starting-project/src/app/tasks/tasks.component.ts
+++ 01-starting-project/src/app/tasks/tasks.component.ts
@@ -1,14 +1,23 @@
 import { Component, Input} from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 import { UserComponent } from '../user/user.component';
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
  @Input({required:true}) name!: string;
+ tasks = [
+  {
+    id: 't1',
+    title: 'Aprender Angular',
+    summary: 'Aprender todas as ferramentas básicas e avançadas do Angular, além de outros formatos de desenvolvimento de aplicativos.',
+    dueDate: '2025-11-30'
+  }
+]
 }
