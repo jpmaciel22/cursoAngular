ffd2de3911de55868ec975a29fcd473b9df631b7
jpmaciel22 <131266912+jpmaciel22@users.noreply.github.com>
2025-02-12 14:21:46 -0300

    adicionado if e for e correções.

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common'
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
--- 01-starting-project/src/app/user/user-antigo.component.ts
+++ 01-starting-project/src/app/user/user-antigo.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal, computed } from '@angular/core';
+import { Component, signal, computed, input, output } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
@@ -11,9 +11,11 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 //   styleUrl: './user.component.css'
 // })
 export class UserComponent {
+
   selectedUser = signal(DUMMY_USERS[randomIndex]) // isso é feito de forma análoga às locals do
   // controller de JS, aq ele exporta as variaveis da userComponent como locals para serem usadas no seu
   // proprio html, que depois sera rodado no app component e por ai vai..
+
 
   imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
   // get imagePath() {
