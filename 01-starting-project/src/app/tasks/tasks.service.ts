9954e84ed79fcab7e7f423706b92b03c37029f3f
jpmaciel22 <131266912+jpmaciel22@users.noreply.github.com>
2025-02-14 09:22:33 -0300

    localstorage implementado !

--- 01-starting-project/src/app/tasks/tasks.service.ts
+++ 01-starting-project/src/app/tasks/tasks.service.ts
@@ -24,7 +24,16 @@ export class TasksService {
     summary: 'Aprender todas as ferramentas básicas e avançadas do Angular, além de outros formatos de desenvolvimento de aplicativos.',
     dueDate: '2025-11-30'
   }
-]
+];
+
+constructor(){
+  const tasks = localStorage.getItem('tasks');
+
+  if(tasks){
+    this.tasks = JSON.parse(tasks)
+  }
+}
+
 getUserTasks(userId: string){
   return this.tasks.filter((task) => task.userId === userId)
 }
@@ -36,8 +45,14 @@ addTask(task: NewTaskData, userId: string){
     dueDate: task.dueDate,
     userId: userId
   })
+  this.saveTasks()
 }
 deleteTask(id: string){
   this.tasks = this.tasks.filter((task) => task.id !== id)
+  this.saveTasks()
+}
+
+private saveTasks(){
+  localStorage.setItem('tasks',JSON.stringify(this.tasks))
 }
 }
