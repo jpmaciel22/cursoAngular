7eada13c8302371c59bb4fdb66a004ddf3098fb3
jpmaciel22 <131266912+jpmaciel22@users.noreply.github.com>
2025-02-18 16:31:15 -0300

    comeco de component inputs repetition

---
+++ 03-deep-dive-components/src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -0,0 +1,12 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-item.component.html',
+  styleUrl: './dashboard-item.component.css'
+})
+export class DashboardItemComponent {
+image = input<{src: string, alt:string}>()
+}
