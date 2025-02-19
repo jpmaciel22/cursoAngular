389ea51a7dc96409b6a1c2fbc90a216926702612
jpmaciel22 <131266912+jpmaciel22@users.noreply.github.com>
2025-02-19 10:50:30 -0300

    changing css elements scope

--- 03-deep-dive-components/src/app/shared/button/button.component.ts
+++ 03-deep-dive-components/src/app/shared/button/button.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'button[appButton]', // isso é para mostrar ao angular que qualquer botao que tem o
+  selector: 'button[appButton], a[appButton]', // isso é para mostrar ao angular que qualquer botao que tem o
   // atributo appButton deve ser controlado por este component
   standalone: true,
   imports: [],
--- 03-deep-dive-components/src/app/shared/control/control.component.ts
+++ 03-deep-dive-components/src/app/shared/control/control.component.ts
@@ -1,11 +1,13 @@
-import { Component, input } from '@angular/core';
+import { Component, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   standalone: true,
   imports: [],
   templateUrl: './control.component.html',
-  styleUrl: './control.component.css'
+  styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None // faz perder o escopo do css para que os elementos invocados com
+  //ngContent recebam o css
 })
 export class ControlComponent {
   label = input.required<string>()
