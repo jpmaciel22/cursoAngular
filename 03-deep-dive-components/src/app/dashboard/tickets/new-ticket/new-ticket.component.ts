dd2be9903c25b34c55bae471b4e1012ea058040a
jpmaciel22 <131266912+jpmaciel22@users.noreply.github.com>
2025-02-20 15:11:12 -0300

    atualizado para signals os template variables com view child

--- 03-deep-dive-components/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 03-deep-dive-components/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, signal, viewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from "../../../shared/control/control.component";
 import { FormsModule } from '@angular/forms';
@@ -11,13 +11,15 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
-  @ViewChild('form') form?: ElementRef<HTMLFormElement>; // aqui é para poder o newTicketComponent 'enxergar' o #form do html
+  // @ViewChild('form') form?: ElementRef<HTMLFormElement>; // aqui é para poder o newTicketComponent 'enxergar' o #form do html
+  private form = viewChild<ElementRef<HTMLFormElement>>('form') // msm coisa porem com signals
+
 
   onSubmit(titleInput: HTMLInputElement, textInput: HTMLTextAreaElement){
     const title = titleInput.value
     const texto = textInput.value
     console.log(title, texto)
-    this.form?.nativeElement.reset(); // nesse caso precisamos do .nativeElement pois o ElementRef
+    this.form()?.nativeElement.reset(); // nesse caso precisamos do .nativeElement pois o ElementRef
     // aponta para o form element que esta fazendo referencia ao elemento do DOM form, porem para acessar O FORM
     //  em si precisamos desse .nativeElement
   }
