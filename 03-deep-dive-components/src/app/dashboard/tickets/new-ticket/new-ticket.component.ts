48a40f605d53f1c62320c63ba8eefa93fd7d733c
jpmaciel22 <131266912+jpmaciel22@users.noreply.github.com>
2025-02-20 16:30:50 -0300

    eventEmitter add input

--- 03-deep-dive-components/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 03-deep-dive-components/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,7 +1,8 @@
-import { Component, ViewChild, ElementRef, signal, viewChild, AfterViewInit } from '@angular/core';
+import { Component, ViewChild, ElementRef, signal, viewChild, AfterViewInit, output, EventEmitter } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from "../../../shared/control/control.component";
 import { FormsModule } from '@angular/forms';
+import { Ticket } from '../ticket/ticket.model';
 
 @Component({
   selector: 'app-new-ticket',
@@ -13,6 +14,7 @@ import { FormsModule } from '@angular/forms';
 export class NewTicketComponent implements AfterViewInit{
   // @ViewChild('form') form?: ElementRef<HTMLFormElement>; // aqui é para poder o newTicketComponent 'enxergar' o #form do html
   private form = viewChild.required<ElementRef<HTMLFormElement>>('form') // msm coisa porem com signals
+  add = output<{title: string, text: string}>()
 
   // é bom de tratar os dados se necessario em ngAfterViewInit e nao em ngOnInit, pois no caso do ngOnInit
   // os dados ainda estariam undefined, ao contrário do que acontece com o afterview
@@ -23,6 +25,7 @@ export class NewTicketComponent implements AfterViewInit{
   onSubmit(titleInput: HTMLInputElement, textInput: HTMLTextAreaElement){
     const title = titleInput.value
     const texto = textInput.value
+    this.add.emit({title: title, text: texto})
     this.form().nativeElement.reset(); // nesse caso precisamos do .nativeElement pois o ElementRef
     // aponta para o form element que esta fazendo referencia ao elemento do DOM form, porem para acessar O FORM
     //  em si precisamos desse .nativeElement
