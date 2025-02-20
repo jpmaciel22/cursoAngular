c2075ed39210bb01445b8dba8adaa74da713a77f
jpmaciel22 <131266912+jpmaciel22@users.noreply.github.com>
2025-02-20 14:24:59 -0300

    implementando ngOnDestroy e ngOnInit

--- 03-deep-dive-components/src/app/dashboard/server-status/server-status.component.ts
+++ 03-deep-dive-components/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,11 +7,14 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
+export class ServerStatusComponent implements OnInit, OnDestroy{
   currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
+  private intervalID?: ReturnType<typeof setInterval> // o tipo de intervalID deveria ser NodeJS.Timeout
+  // porém o angular nao está reocnhecendo, então utiliazmos o typescript para informar que
+  // o seu type deve ser o tipo retornado pela funcao setInterval que é esse NodeJS.Timeout
 
-  constructor(){
-    setInterval(() => {
+  ngOnInit(){ // é melhor utilizar o ngOnInit do que o constructor em Angular.
+    this.intervalID = setInterval(() => {
       const random = Math.random();
       if(random < 0.5){
         this.currentStatus = 'online'
@@ -22,4 +25,9 @@ export class ServerStatusComponent {
       }
     }, 5000)
   }
+
+  ngOnDestroy(){
+    clearTimeout(this.intervalID);
+  }
+
 }
