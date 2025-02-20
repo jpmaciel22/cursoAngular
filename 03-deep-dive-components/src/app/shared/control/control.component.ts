2fc1a37a18346f9146f7d84e6e6642f50f529a74
jpmaciel22 <131266912+jpmaciel22@users.noreply.github.com>
2025-02-20 15:30:13 -0300

    ContentChild implementação

--- 03-deep-dive-components/src/app/shared/control/control.component.ts
+++ 03-deep-dive-components/src/app/shared/control/control.component.ts
@@ -5,7 +5,9 @@ import {
   input,
   ViewEncapsulation,
   inject,
-  ElementRef } from '@angular/core';
+  ElementRef,
+  ContentChild,
+  contentChild} from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -28,9 +30,17 @@ export class ControlComponent {
 
   label = input.required<string>()
   private el = inject(ElementRef) // parecido com o target do js html dom nativo
+  // @ContentChild('input') private control?:ElementRef<HTMLInputElement | HTMLTextAreaElement>
+  private control = contentChild.required<ElementRef<HTMLInputElement|HTMLTextAreaElement>>('input')
+  // assim que se acha conteudos projetados, utilizando ng-content, no caso de serem
+  //  inputs seria mais facil, mas para fazer referencia direta enxergando é assim.
+  //  para checar viewChilds checar o new-ticket
+
+
   onclick(){
     console.log('Click')
     console.log(this.el)
+    console.log(this.control())
   }
 
 }
