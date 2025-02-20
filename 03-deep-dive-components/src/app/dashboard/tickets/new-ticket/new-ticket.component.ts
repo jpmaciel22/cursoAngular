0151177e268ab310fb266df88d3d998540adbabb
jpmaciel22 <131266912+jpmaciel22@users.noreply.github.com>
2025-02-20 14:44:59 -0300

    new ticket implementando outra forma de two way binding

--- 03-deep-dive-components/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 03-deep-dive-components/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from "../../../shared/control/control.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
-
+  onSubmit(titleInput: HTMLInputElement, textInput: HTMLTextAreaElement){
+    const title = titleInput.innerText
+    const texto = textInput.innerText
+    console.log(title, texto)
+  }
 }
