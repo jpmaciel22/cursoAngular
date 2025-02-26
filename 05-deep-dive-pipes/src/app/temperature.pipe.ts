8523a9f657ee6a6f0a49783f51e2f8fc2bda4116
jpmaciel22 <131266912+jpmaciel22@users.noreply.github.com>
2025-02-26 16:31:33 -0300

    pipe completamente funcional agora com parametros custom também

--- 05-deep-dive-pipes/src/app/temperature.pipe.ts
+++ 05-deep-dive-pipes/src/app/temperature.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from "@angular/core";
   standalone: true
 })
 export class TemperaturePipe implements PipeTransform{
-transform(value:number | string, ...args: any[]){
+transform(value:number | string, ...args:any[]){
   let val: number;
   if(typeof value === 'string'){
     val = Number(value);
@@ -14,6 +14,11 @@ transform(value:number | string, ...args: any[]){
   }
 
   const outputTemp = val * (9/5) + 32
+
+  if(args[0] === 'fahrenheit'){
+    const outputToCelsius = (val-32) * 5/9
+    return outputToCelsius.toFixed(2)
+  }
 
   return `${outputTemp.toFixed(2)} °F`;
 }
