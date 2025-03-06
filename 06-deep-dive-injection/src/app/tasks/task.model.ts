9f496ab9a534033b203455ef3a7b008ec3641036
jpmaciel22 <131266912+jpmaciel22@users.noreply.github.com>
2025-03-06 15:10:53 -0300

    taskStatusOptions adicionado non class injection

--- 06-deep-dive-injection/src/app/tasks/task.model.ts
+++ 06-deep-dive-injection/src/app/tasks/task.model.ts
@@ -1,5 +1,23 @@
 export type TaskStatus = 'OPEN' | 'IN_PROGRESS' | 'DONE';
 
+export const TaskStatusOptions = [
+  {
+    value: 'open',
+    taskStatus: 'OPEN',
+    text: 'Open'
+  },
+  {
+    value: 'in-progress',
+    taskStatus: 'IN_PROGRESS',
+    text: 'Working on it'
+  },
+  {
+    value: 'done',
+    taskStatus: 'DONE',
+    text: 'Completed'
+  }
+]
+
 export interface Task {
   id: string;
   title: string;
