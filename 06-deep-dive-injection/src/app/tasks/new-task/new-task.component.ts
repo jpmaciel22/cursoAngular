044b14a77dd79b87d1d04eb83fba51cec2a4e023
jpmaciel22 <131266912+jpmaciel22@users.noreply.github.com>
2025-03-06 14:58:26 -0300

    implementado um custom token de services..

--- 06-deep-dive-injection/src/app/tasks/new-task/new-task.component.ts
+++ 06-deep-dive-injection/src/app/tasks/new-task/new-task.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, viewChild, inject} from '@angular/core';
+import { Component, ElementRef, viewChild, inject, Inject} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { TasksService } from '../tasks.service';
 import { TasksServiceToken } from '../../../main';
@@ -14,7 +14,7 @@ export class NewTaskComponent {
   private formEl = viewChild<ElementRef<HTMLFormElement>>('form');
 
   // constructor(private tasksService: TasksService){} // uma forma de injetar o servico em angular
-  private tasksService = inject(TasksServiceToken)
+  constructor(@Inject(TasksServiceToken) private tasksService: TasksService){} // uma forma de injetar o servico em angular com custom token
 
   onAddTask(title: string, description: string) {
     this.tasksService.addTask({title: title, description: description})
