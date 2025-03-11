4ebd8611fb29654d50e655bc82547162f24e2de1
jpmaciel22 <131266912+jpmaciel22@users.noreply.github.com>
2025-03-11 14:32:00 -0300

    Começo de RXJS observables..

--- 08-rxjs-deep-dive/src/app/app.component.ts
+++ 08-rxjs-deep-dive/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html'
 })
-export class AppComponent {
-
+export class AppComponent implements OnInit{
+  ngOnInit():void{
+    interval(1000).subscribe({ // o subscribe é importante para iniciar em si o observable, sem ele o interval não rodaria.
+      next: (value) => {console.log(value)},
+      complete: () => {} // executa quando completa..
+    });
+  }
 }
