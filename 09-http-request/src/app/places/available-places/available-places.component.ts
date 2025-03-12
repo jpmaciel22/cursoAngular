4be781fd5e211696b1a4d8e4bb52bacace44f101
jpmaciel22 <131266912+jpmaciel22@users.noreply.github.com>
2025-03-12 10:35:28 -0300

    Lidando com erros HTTP

--- 09-http-request/src/app/places/available-places/available-places.component.ts
+++ 09-http-request/src/app/places/available-places/available-places.component.ts
@@ -4,7 +4,7 @@ import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs';
+import { catchError, map, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -15,16 +15,24 @@ import { map } from 'rxjs';
 })
 export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false);
+  error = signal('')
   private httpClient = inject(HttpClient) // para fazer http requests com angular
   private destroyRef = inject(DestroyRef)
   ngOnInit(): void {
+    this.isFetching.set(true)
     const subscription = this.httpClient.get<{places: Place[]}>('http://localhost:3000/places')
-    .pipe(map((response) => response.places))
-    .subscribe({
+    .pipe(map((response) => response.places),
+          catchError((error) => throwError(() => new Error(error.message))))
+    .subscribe({ // este pipe poderia ser ignorado, mas para termos mais exemplos estarei usando aqui.
       next: (places) => {
         // this.places.update((array) => array = places.places) // neste caso sem o pipe
         this.places.update((array) => array = places)
       },
+      complete: () => {this.isFetching.set(false)},
+      error: (error) => {
+        this.error.set(error.message)
+      }
     });
     this.destroyRef.onDestroy(() => {
       subscription.unsubscribe()
