faae806a7f1e21a0c3ec69979419bfb49d7472c8
jpmaciel22 <131266912+jpmaciel22@users.noreply.github.com>
2025-03-12 11:02:41 -0300

    UserPlaces Favorite Places funcionando

--- 09-http-request/src/app/places/user-places/user-places.component.ts
+++ 09-http-request/src/app/places/user-places/user-places.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, signal, inject, OnInit, DestroyRef } from '@angular/core';
 
-import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
+import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-user-places',
@@ -10,5 +13,30 @@ import { PlacesComponent } from '../places.component';
   styleUrl: './user-places.component.css',
   imports: [PlacesContainerComponent, PlacesComponent],
 })
-export class UserPlacesComponent {
+export class UserPlacesComponent implements OnInit{
+   places = signal<Place[] | undefined>(undefined);
+    isFetching = signal(false);
+    error = signal('')
+    private httpClient = inject(HttpClient) // para fazer http requests com angular
+    private destroyRef = inject(DestroyRef)
+
+  ngOnInit(): void {
+      this.isFetching.set(true)
+      const subscription = this.httpClient.get<{places: Place[]}>('http://localhost:3000/user-places')
+      .pipe(map((response) => response.places),
+            catchError((error) => throwError(() => new Error(error.message))))
+      .subscribe({ // este pipe poderia ser ignorado, mas para termos mais exemplos estarei usando aqui.
+        next: (places) => {
+          // this.places.update((array) => array = places.places) // neste caso sem o pipe
+          this.places.update((array) => array = places)
+        },
+        complete: () => {this.isFetching.set(false)},
+        error: (error) => {
+          this.error.set(error.message)
+        }
+      });
+      this.destroyRef.onDestroy(() => {
+        subscription.unsubscribe()
+      })
+    }
 }
