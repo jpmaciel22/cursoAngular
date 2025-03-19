a02ea5436471a6e8f375a3dd4a120da785bd7737
jpmaciel22 <131266912+jpmaciel22@users.noreply.github.com>
2025-03-19 10:43:57 -0300

    Salvando o form em local storage

--- 10-forms/src/app/auth/login/login.component.ts
+++ 10-forms/src/app/auth/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { afterNextRender, Component, DestroyRef, inject, viewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
@@ -9,6 +9,22 @@ import { FormsModule, NgForm } from '@angular/forms';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  constructor(){
+    afterNextRender(() => {
+      const subscription = this.form()?.valueChanges?.subscribe({
+        next: (value) => {
+          window.localStorage.setItem('saved-login-form',JSON.stringify(
+            {email: value.email, password: value.password}));
+        }
+      });
+      this.destroyRef.onDestroy(() => subscription?.unsubscribe())
+    });
+
+  }
+  private destroyRef = inject(DestroyRef)
+  private form = viewChild<NgForm>('form') // acessando o #form do template html, o mesmo form acessado na
+  // funcao abaixo
+
   onSubmit(form:NgForm){
     if(!form.form.valid){
       return;
@@ -17,5 +33,7 @@ export class LoginComponent {
     const emailInput = form.form.value.email
     const passInput = form.form.value.password
     console.log(passInput, emailInput)
+
+    form.form.reset();
   }
 }
