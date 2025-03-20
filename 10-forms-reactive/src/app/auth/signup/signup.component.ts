d6e8ada990052f9d228f1ee1b77db0525f00139e
jpmaciel22 <131266912+jpmaciel22@users.noreply.github.com>
2025-03-20 15:57:02 -0300

    FormArray

--- 10-forms-reactive/src/app/auth/signup/signup.component.ts
+++ 10-forms-reactive/src/app/auth/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { debounceTime, of } from 'rxjs';
 
 @Component({
@@ -35,12 +35,17 @@ export class SignupComponent {
     dropdown: new FormControl<'student' | 'teacher' | 'employee' | 'founder' | 'other'>('student',
       {validators: [Validators.required]}
      ),
+    source: new FormArray([
+      new FormControl(false),
+      new FormControl(true),
+      new FormControl(false),
+    ]),
     agree: new FormControl<boolean>(false, {validators: [Validators.required]}),
 
   })
   onSubmit(){
     console.log(this.NossoForm)
-    // this.onReset()
+    this.onReset()
   }
   get emailInvalid(){
     return(this.NossoForm.controls.email.touched && this.NossoForm.controls.email.dirty && !this.NossoForm.controls.email.valid)
