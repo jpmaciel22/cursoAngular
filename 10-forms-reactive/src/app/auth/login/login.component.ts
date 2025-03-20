a847d6e677d853d1d147775e1857447babf4f7c3
jpmaciel22 <131266912+jpmaciel22@users.noreply.github.com>
2025-03-20 14:17:02 -0300

    Implementado localStorage novamente

--- 10-forms-reactive/src/app/auth/login/login.component.ts
+++ 10-forms-reactive/src/app/auth/login/login.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { debounceTime, of } from 'rxjs';
 
 function necessitaInterrogacao(control: AbstractControl){
   if(control.value.includes('?')){
     return null;
   }
   return { naoContemInterrogacao: true };
+}
+
+function emailUnico(control: AbstractControl){
+  if(control.value !== 'testezudo@gmail.com'){
+    return of(null)
+  }
+  return of({emailsIguais: true})
 }
 
 @Component({
@@ -15,10 +23,30 @@ function necessitaInterrogacao(control: AbstractControl){
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+
+ngOnInit(): void {
+  const savedForm = window.localStorage.getItem('saved-login-form');
+  if(savedForm){
+    const loadedForm = JSON.parse(savedForm);
+    this.NossoForm.setValue({email: loadedForm.email, password: loadedForm.password})
+  }
+
+
+  const subscription = this.NossoForm.valueChanges.pipe(debounceTime(100)).subscribe({
+    next: value => {
+      window.localStorage.setItem('saved-login-form', JSON.stringify({email: value.email, password: value.password}));
+    }
+  });
+  this.destroyRef.onDestroy(() => {
+    subscription.unsubscribe();
+  })
+}
+  private destroyRef = inject(DestroyRef);
   NossoForm = new FormGroup({
     email: new FormControl('', {
-      validators: [Validators.email, Validators.required]
+      validators: [Validators.email, Validators.required],
+      asyncValidators: [emailUnico]
     }),
     password: new FormControl('', {
       validators: [Validators.required, Validators.minLength(6), necessitaInterrogacao]
