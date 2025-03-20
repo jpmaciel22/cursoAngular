0cee3b5368c38b7e925a26d3699182dd21242345
jpmaciel22 <131266912+jpmaciel22@users.noreply.github.com>
2025-03-20 17:03:39 -0300

    Fazendo as configurações de routing

--- 11-routing/src/main.ts
+++ 11-routing/src/main.ts
@@ -2,13 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 
 import { AppComponent } from './app/app.component';
-import { TasksComponent } from './app/tasks/tasks.component';
+import { routes } from './app/app.routes';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent,
-   {providers: [provideRouter([
-    {
-      path: 'tasks',
-      component: TasksComponent
-    }
-   ])],
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
