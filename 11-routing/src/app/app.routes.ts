62779c7fb564d98442eb49aadbbe8e7c6b1f5f93
jpmaciel22 <131266912+jpmaciel22@users.noreply.github.com>
2025-03-21 15:11:17 -0300

    user tasks e nested route links

--- 11-routing/src/app/app.routes.ts
+++ 11-routing/src/app/app.routes.ts
@@ -1,15 +1,27 @@
 import { Routes } from "@angular/router"
 import { NoTaskComponent } from "./tasks/no-task/no-task.component"
 import { UserTasksComponent } from "./users/user-tasks/user-tasks.component"
+import { TasksComponent } from "./tasks/tasks.component"
+import { NewTaskComponent } from "./tasks/new-task/new-task.component"
 
 export const routes: Routes =
 [
   {
-    path: '', //localhost:4200
+    path: '', //localhost:4200/
     component: NoTaskComponent
   },
   {
-    path: 'users/:userId', // localhost:4200/users/(o user id)
-    component: UserTasksComponent
+    path: 'users/:userId', // localhost:4200/users/(o user id)/
+    component: UserTasksComponent,
+    children: [
+      {
+        path: 'tasks', //localhost:4200/users/(o user id)/tasks/
+        component: TasksComponent
+      },
+      {
+        path: 'tasks/new',
+        component: NewTaskComponent
+      }
+    ]
   },
    ]
--- 11-routing/src/app/users/user-tasks/user-tasks.component.ts
+++ 11-routing/src/app/users/user-tasks/user-tasks.component.ts
@@ -1,12 +1,13 @@
 import { Component, computed, inject, input, OnInit } from '@angular/core';
 import { UsersService } from '../users.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-user-tasks',
   standalone: true,
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
+  imports: [RouterOutlet, RouterLink]
 })
 export class UserTasksComponent{
   userId = input.required<string>(); // ele consegue esse input a partir da variavel do route
