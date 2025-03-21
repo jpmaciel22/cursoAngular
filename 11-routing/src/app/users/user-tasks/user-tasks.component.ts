68611c372dda4a42f6bea4d9f8708e2de6d8da07
jpmaciel22 <131266912+jpmaciel22@users.noreply.github.com>
2025-03-21 14:56:35 -0300

    Dynamic route parameter pelo input e exemplo comentado usando observables

--- 11-routing/src/app/users/user-tasks/user-tasks.component.ts
+++ 11-routing/src/app/users/user-tasks/user-tasks.component.ts
@@ -1,5 +1,6 @@
-import { Component, computed, inject, input } from '@angular/core';
+import { Component, computed, inject, input, OnInit } from '@angular/core';
 import { UsersService } from '../users.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-tasks',
@@ -7,11 +8,18 @@ import { UsersService } from '../users.service';
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent {
+export class UserTasksComponent{
   userId = input.required<string>(); // ele consegue esse input a partir da variavel do route
   // ja que foi ativado a partir do app config.
   private usersService = inject(UsersService);
-
   userName = computed(() => this.usersService.users.find((user) => user.id === this.userId())?.name);
+  // private activatedRoute = inject(ActivatedRoute);
+  // userName = ''
 
+
+  // ngOnInit(): void {
+  //   this.activatedRoute.paramMap.subscribe({
+  //     next: paramMap => this.userName = this.usersService.users.find( u => u.id === paramMap.get('userId'))?.name || ''
+  //   })
+  // }
 }
