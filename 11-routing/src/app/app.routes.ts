00e1c4b47a0451c9acf5a525bb75a11f397e9348
jpmaciel22 <131266912+jpmaciel22@users.noreply.github.com>
2025-03-21 14:05:17 -0300

    Trabalhando com routerlinks

--- 11-routing/src/app/app.routes.ts
+++ 11-routing/src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from "@angular/router"
 import { TasksComponent } from "./tasks/tasks.component"
+import { NoTaskComponent } from "./tasks/no-task/no-task.component"
 
 export const routes: Routes =
 [
-    {
-      path: 'tasks',
+  {
+    path: '', //localhost:4200
+    component: NoTaskComponent
+  },
+  {
+      path: 'tasks', //localhost:4200/tasks
       component: TasksComponent
     }
    ]
