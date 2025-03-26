33ddd630095cca641ae93df749bfcf57feb31668
jpmaciel22 <131266912+jpmaciel22@users.noreply.github.com>
2025-03-26 09:48:03 -0300

    tasks funcionais novamente, com nested routes e parent data

--- 11-routing/src/app/app.config.ts
+++ 11-routing/src/app/app.config.ts
@@ -1,7 +1,11 @@
 import { ApplicationConfig } from "@angular/core";
-import { provideRouter, withComponentInputBinding } from "@angular/router";
+import { provideRouter, withComponentInputBinding, withRouterConfig } from "@angular/router";
 import { routes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withComponentInputBinding()),],
+  providers: [provideRouter(routes, withComponentInputBinding(),
+    withRouterConfig({
+      paramsInheritanceStrategy: 'always', // isso permite que os parametros dinamicos como o 'userId' de
+      //user-tasks seja injetado também em child components
+    })),],
 }
