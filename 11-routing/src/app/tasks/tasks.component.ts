d855b5043fa6dfa4f89de161b6bcf54151bc5eab
jpmaciel22 <131266912+jpmaciel22@users.noreply.github.com>
2025-03-26 11:31:18 -0300

    Funcao de sort pronta sortando as datas das tasks

--- 11-routing/src/app/tasks/tasks.component.ts
+++ 11-routing/src/app/tasks/tasks.component.ts
@@ -13,18 +13,28 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
   imports: [TaskComponent, RouterLink],
 })
 export class TasksComponent {
-  // order = input<'asc' | 'desc'>();
+  order = input<'asc' | 'desc'>();
   userId = input.required<string>();
   private tasksService = inject(TasksService)
-  userTasks = computed(() => this.tasksService.allTasks().filter((task) => task.userId === this.userId()));
+  userTasks = computed(
+    () => this.tasksService.allTasks()
+    .filter((task) => task.userId === this.userId())
+    .sort((a,b) => {
+      if(this.order() === 'desc'){
+        return a.id > b.id ? -1:1
+      }else {
+        return a.id > b.id? 1:-1
+      }
+    })
+  );
 
-  private activatedRoute = inject(ActivatedRoute);
-  private destroyRef = inject(DestroyRef)
-  order?: 'asc'|'desc';
-  ngOnInit(): void{
-    const subscription = this.activatedRoute.queryParams.subscribe({
-      next: params => this.order = params['order'],
-    });
-    this.destroyRef.onDestroy(() => subscription.unsubscribe())
-  }
+  // private activatedRoute = inject(ActivatedRoute);
+  // private destroyRef = inject(DestroyRef)
+  // order = signal<'asc'|'desc'>('desc')
+  // ngOnInit(): void{
+  //   const subscription = this.activatedRoute.queryParams.subscribe({
+  //     next: (params) => (this.order.set(params['order'])),
+  //   });
+  //   this.destroyRef.onDestroy(() => subscription.unsubscribe())
+  // }
 }
